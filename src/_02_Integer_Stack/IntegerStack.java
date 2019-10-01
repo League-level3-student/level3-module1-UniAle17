b3636f9752d16ba9be17cfa7341c13ca88370c6d
ilabstaff <ilabstaff@172.16.116.128>
2019-09-30 17:48:03 -0700

    IntegerStack IntroToStack

--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -3,20 +3,38 @@ package _02_Integer_Stack;
 public class IntegerStack {
 	//1. create a private array of integers
 	
+	private int [] member;
+	
+	
 	//2. complete the constructor by initializing the member array
 	//   to a new array of length 0. This prevents null pointer exceptions.
 	public IntegerStack() {
+		
+		member= new int [0];
+		
 	}
 	
 	//3. Complete the steps in the push method.
 	public void push(int v) {
 		//A. create a new array that is one element larger than the member array
 		
+		int [] bigger = new int [member.length + 1];
+		
+		
 		//B. set the last element of the new array equal to the integer passed into the method
+		
+		bigger[bigger.length-1] = v;
 		
 		//C. iterate through the member array and copy all the elements into the new array
 		
+		for (int i = 0; i < member.length; i++) {
+			bigger[i]= member[i];
+		}
+			
+				
 		//D. set the member array equal to the new array.
+		
+		member = bigger;
 		
 	}
 	
@@ -25,26 +43,40 @@ public class IntegerStack {
 		//A. create an integer variable and initialize it to the
 		//   last element of the member array.
 		
+		int c = member.length -1;
+		
 		//B. create a new array that is one element smaller than the member array
+		
+		int [] smaller = new int [member.length-1];
 		
 		//C. iterate through the new array and copy every element from the
 		//   member array to the new array
 		
+		for (int i = 0; i < smaller.length; i++) {
+			smaller[i] = member[i];
+		}
+		
 		//D. set the member array equal to the new array
 		
+		member=smaller;		
+		
 		//E. return the variable you created in step A
-		return 0;
+		return c;
 	}
 	
 	//5. Complete the clear method to set the
 	//   member array to a new array of length 0
 	public void clear() {
 		
+		int []zero = new int [0];
+		
+		member=zero;
+		
 	}
 	
 	//6. Complete the size method to return 
 	//   the length of the member array
 	public int size() {
-		return 0;
+		return member.length;
 	}
 }
--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -4,13 +4,23 @@ import java.util.Random;
 import java.util.Stack;
 
 import javax.swing.JOptionPane;
+import java.util.Stack;
+
 
 public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
 		
+		Stack<Double> a = new Stack<Double>();
+		
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+		
+		for (int i = 0; i < 99; i++) {
+			
+			
+			
+		}
 		
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
 		
