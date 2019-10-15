cf737fe45e1b0279ea550141096ceddcf21eb254
ThirdTime <esmepuzio@gmail.com>
2019-10-14 17:45:38 -0700

    TextUndoRedo

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,10 +1,13 @@
 package _03_IntroToStacks;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class _02_TextUndoRedo {
+public class _02_TextUndoRedo implements KeyListener{
 	
 	
 	JFrame frame = new JFrame();
@@ -18,12 +21,55 @@ public class _02_TextUndoRedo {
 		
 		frame.setVisible(true);
 		frame.pack();
+		frame.addKeyListener(this);
+		
+		
 		
 	}
 	
 	public static void main(String[] args) {
 		
 	_02_TextUndoRedo runcode = new _02_TextUndoRedo();
+		
+	
+	
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		char c = e.getKeyChar();
+		
+		String l = label.getText();
+		l += c;
+		
+		label.setText(l);
+		
+		String deleteLabel = l.substring(0, l.length()- 1);
+		
+		if(e.getKeyCode()==KeyEvent.VK_BACK_SPACE) {
+			
+			l=deleteLabel;
+			
+			label.setText(l);
+			
+			
+			
+		}
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 	
