6858f7463482a4b358869bc287135e1850c597fe
ilabstaff <ilabstaff@172.16.116.128>
2019-10-28 17:43:06 -0700

    Hang=Man

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,59 @@
 package _04_HangMan;
 
-public class HangMan{
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class HangMan implements KeyListener{
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label= new JLabel();
+	
+	HangMan(){
+		
+		frame.add(panel);
+		panel.add(label);
+		
+		frame.setVisible(true);
+		frame.pack();
+		frame.addKeyListener(this);
+		 JOptionPane.showInputDialog("Welcome to Hangman! "
+		 		+ "How many words would you like to attempt?");
+		
+	}
 	
 	
+	public static void main(String[] args) {
+		
+		HangMan hm = new HangMan();
+		
+		
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
